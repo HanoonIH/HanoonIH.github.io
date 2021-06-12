0f124c11f7976e985398488349783b79461646fa
HanoonIH <hanoonih123@gmail.com>
2021-06-12 16:48:03 +0530

    Hero text change, mobile font sizes decreased, services icon added

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 window.onload = () => {
 
+    // Menu & Nav screen
     var menuIcon = document.querySelector('#menuIcon');
     var closeIcon = document.querySelector('#closeIcon');
     var navScreen = document.getElementById('navScreen');
@@ -9,12 +10,8 @@ window.onload = () => {
     closeIcon.addEventListener('click', closeNavScreen);
     navItems.forEach(item => {
         item.addEventListener('click', closeNavScreen);
-    }) 
+    });
 
-    // if (closeIcon) {   
-    //     closeIcon.addEventListener('click', closeNavScreen);
-    // }
-    
     function openNavScreen() {
         navScreen.style.visibility = 'visible';
         menuIcon.style.display = 'none';
@@ -27,4 +24,18 @@ window.onload = () => {
         closeIcon.style.display = 'none';
         menuIcon.style.display = 'block';
     }
+
+    // Hire button text change in mobile
+    var mobileScreen = window.matchMedia("(max-width: 425px)");
+    function changeText(mobileScreen) {
+        if(mobileScreen.matches) {
+            document.getElementById('hireBtn').innerHTML = 'Hire';
+        } else {
+            document.getElementById('hireBtn').innerHTML = 'Hire Me';
+
+        
+        }
+    }
+    changeText(mobileScreen);
+    mobileScreen.addListener(changeText);
 }
